660d3606e50b56d4c20e8a59f5b5dd0fbffb18b2
yuchen9902 <yuchenz8@illinois.edu>
2019-04-20 17:02:17 -0500

    api

--- app/src/main/java/com/example/lovecalculator/MainActivity.java
+++ app/src/main/java/com/example/lovecalculator/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.lovecalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+import unirest.HttpResponse;
+import unirest.JsonNode;
+import unirest.Unirest;
+
+
+public class MainActivity extends AppCompatActivity {
+    HttpResponse<JsonNode> response = Unirest.get("https://love-calculator.p.rapidapi.com/getPercentage?fname=John&sname=Alice")
+            .header("X-RapidAPI-Host", "love-calculator.p.rapidapi.com")
+            .header("X-RapidAPI-Key", "ddb2ddfd21msh545fdbc2222d20dp1ba2f6jsn6d9725535c4a").asJson();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
